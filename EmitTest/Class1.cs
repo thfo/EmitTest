e55f22667ceca752ec6b358fce99554bb21e625b
Eddie Chen <173135281@qq.com>
2017-02-01 18:09:10 +0800

    haha

---
+++ EmitTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmitTest
+{
+    class Class1
+    {
+        public static string s = "";
+    }
+}
